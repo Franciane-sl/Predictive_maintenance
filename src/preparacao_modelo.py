4db323c7ce4096fd989ac448d6572936db93b30b
Franciane Sl <franciane.leitee@gmail.com>
2026-07-19 10:48:17 -0300

    feat: adiciona função para escalonamento de variáveis com StandardScaler

--- src/preparacao_modelo.py
+++ src/preparacao_modelo.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
 
 # Função para separar as variáveis independentes (X) da variável dependente (y).
 def separar_variaveis(df):
@@ -73,3 +74,18 @@ def aplicar_smote(X_train, y_train):
     )
 
     return X_train_balanceado, y_train_balanceado
+
+# Função para aplicar o StandardScaler nas variáveis do modelo KNN.
+def escalonar_variaveis(X_train, X_test):
+
+    scaler = StandardScaler()
+
+    X_train_escalonado = scaler.fit_transform(
+        X_train
+    )
+
+    X_test_escalonado = scaler.transform(
+        X_test
+    )
+
+    return X_train_escalonado, X_test_escalonado
